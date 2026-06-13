7798405aef6d58dc24b2b3e40f71b03a1db36a2d
Vinam Sharma <vinamsharmacu@gmail.com>
2026-06-14 03:46:21 +0530

    First Browser Launch Via Code

--- src/main/java/day2/LaunchBrowser.java
+++ src/main/java/day2/LaunchBrowser.java
@@ -1,8 +1,23 @@
 package day2;
 
+import java.nio.file.Paths;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType.LaunchOptions;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
 public class LaunchBrowser {
 
-	public static void Main(String[] args) {
+	public static void main(String[] args) {
+		
+		Playwright playwright = Playwright.create();
+		Browser browser = playwright.chromium().launch(
+				new LaunchOptions().setHeadless(false)
+				.setExecutablePath(Paths.get("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"))
+				);
+		Page page = browser.newPage();
+		page.navigate("https://www.goindigo.in/");
 		
 	}
 	
